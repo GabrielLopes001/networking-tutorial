5133e01cd17bcd7716202e8b25c1a61d7aa1d0b6
Gabriel Lopes <gabriejob1@outlook.com>
2025-01-06 20:44:29 -0300

    Initial Commit

---
+++ networking-tutorial/networking_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  networking_tutorialApp.swift
+//  networking-tutorial
+//
+//  Created by Gabriel Lopes on 06/01/25.
+//
+
+import SwiftUI
+
+@main
+struct networking_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
